23f2b4a0b5bcce89f4a598fb5b2e97e6e95e3be1
edward101992 <edwardconw@gmail.com>
2019-06-03 00:29:18 -0500

    Sistema de Rutas Video 3

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,44 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/usuarios/{id}', function($id){
+	return "El usuario numero : {$id}";
+});
+*/
+
+Route::get('/usuarios/{id}', function($id){
+	return "El usuario numero : {$id}";
+})->where('id','[0-9]+');
+
+
+Route::get('/usuarios/crear',function(){
+	return "Aqui se crea el usuario";
+});
+
+
+Route::get('/usuarios/{nombre}/{apellido}/{edad}',function($nombre,$apellido,$edad){
+	return "Hola usuario {$nombre} {$apellido} de {$edad}";
+})->where('edad','[0-9]+');
+
+
+Route::get('/usuarios/{nombre}/{apellido}/{apellido2}',function($nombre,$apellido,$apellido2){
+	return "Hola {$nombre} {$apellido} {$apellido2}";
+});
+
+Route::get('/user/{id}',function($id){
+	return "Id de Usuario {$id}";
+})->where(['id' => '[\d]+']);
+
+Route::get('/user/{peticion}',function($peticion){
+	return "La peticion fue : {$peticion}";
+})->where(['peticion'=> 'editar|eliminar|insertar']);
+
+Route::get('/user/{name}',function($name){
+	return "Nombre de Usuario {$name}";
+})->where(['name'=> '[-\w]+']);
+
+
+
+
